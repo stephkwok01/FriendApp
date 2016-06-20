bac4dd01a099e45482a7421e54268cac3bebfad3
Stephanie <stephkwok94@gmail.com>
2016-06-20 21:43:51 +0200

    login done and that's about it

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ app.config(function($routeProvider) {
 		templateUrl: "templates/login.html"
 	})
   //home page
-  	$routeProvider.when("/home",{
-  		templateUrl: "templates/home.html"
-  	})
+  $routeProvider.when("/home",{
+  	templateUrl: "templates/home.html"
+  })
 }); //end of config
 
 //login page controller
 app.controller("loginCtrl", function($scope,$location,$firebaseAuth,$http){
-
+	// adding background images
 	var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg',
 	'13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg' ];
 	var randomNumber = Math.floor(Math.random() * images.length);
@@ -21,6 +21,7 @@ app.controller("loginCtrl", function($scope,$location,$firebaseAuth,$http){
 	$scope.backgroundImage = {
 		'background-image' :'url(./Images/'+ randomNumber + '.jpg)',
 	}
+
 	//checking if user is signed in or not
 	var auth = $firebaseAuth();
 	auth.$onAuthStateChanged(function(firebaseUser){
@@ -34,40 +35,57 @@ app.controller("loginCtrl", function($scope,$location,$firebaseAuth,$http){
 		auth.$signInWithPopup("facebook").catch(function(error){
 			console.log("Error");
 		})
-	}
-
-	
+	};
 }); //end of loginCtrl
 
 //home page controller
 app.controller("homeCtrl", function($scope, $http, $location, $firebaseAuth, $firebaseArray){
-	
+
 	//checking if user is signed in or not
 	var auth = $firebaseAuth();
 	auth.$onAuthStateChanged(function(firebaseUser) {
-    if (firebaseUser) {
-      $scope.firebaseUser = firebaseUser;
-      console.log(firebaseUser);
-    } else {
-      console.log(firebaseUser);
-      $location.path("/");
-    }
-  });
+		if (firebaseUser) {
+			$scope.firebaseUser = firebaseUser;
+			console.log($scope.firebaseUser);
+		}
+		else {
+			$location.path("/");
+		}
+	});
+
+	//making arrays of objects in firebase
+	var friendRef = firebase.database().ref().child("friends");
+
+	// var friendObj = {
+	// 	displayName : $scope.firebaseUser.displayName,
+	// 	uid : $scope.firebaseUser.uid,
+	// 	photo : $scope.firebaseUser.photoURL,
+	// };
+	// 	friendRef.push(friendObj);
 
 	//sign out button 
 	$scope.logout = function() {
-    auth.$signOut();
-    $location.path("/");
-  }
+		auth.$signOut();
+		$location.path("/");
+	}
 }); //end of homeCtrl 
 
 
 
 
 
-
-
-
+			// friendRef.once("value", function(snapshot){
+			// 	snapshot.forEach(function(childSnapshot){
+			// 		if(childSnapshot.val().uid === $scope.firebaseUser.uid) {
+			// 			count ++;
+			// 			console.log(count);
+			// 		}
+			// 		if (count === 0) {
+					
+			// 	} 
+			// 	});
+			// });
+				
 
 
 
