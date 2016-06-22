48b6caf3dbc1c7c8206123c23165d42cd82b6fa1
Stephanie <stephkwok94@gmail.com>
2016-06-22 11:08:32 +0200

    to make sure it pulls properly

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
 var app = angular.module("FriendApp", ["ngRoute", "firebase"]);
 
-
-//attempt 2 at this bullshit
-// app.factory('facebookService', function($q) {
-// 	return {
-// 		getMyLastName: function() {
-// 			var deferred = $q.defer();
-// 			FB.api('/me', {
-// 				fields: 'last_name'
-// 			}, function(response) {
-// 				if (!response || response.error) {
-// 					console.log("where am i")
-// 					deferred.reject('Error occured');
-// 				} else {
-// 					deferred.resolve(response);
-// 				}
-// 			});
-// 			return deferred.promise;
-// 		}
-// 	}
-// });
-
-//end of this shitty attmept
-
 //config
 app.config(function($routeProvider) {
 	$routeProvider.when("/", {
@@ -34,49 +11,13 @@ app.config(function($routeProvider) {
   })
 }); //end of config
 
-//attempt 2 at this bullshit
-// app.factory('facebookService', function($q) {
-// 	return {
-// 		getMyLastName: function() {
-// 			var deferred = $q.defer();
-// 			FB.api('/me', {
-// 				fields: 'last_name'
-// 			}, function(response) {
-// 				if (!response || response.error) {
-// 					console.log("where am i")
-// 					deferred.reject('Error occured');
-// 				} else {
-// 					deferred.resolve(response);
-// 				}
-// 			});
-// 			return deferred.promise;
-// 		}
-// 	}
-// });
-
-//end of this shitty attmept
-
-
-
-
 //login page controller
 app.controller("loginCtrl", function($scope,$location,$firebaseAuth,$http,$window){
-	//Facebook API
-	$window.fbAsyncInit = function() {
-   FB.init({ 
-     appId: '{your-app-id}',
-     status: true, 
-     cookie: true, 
-     xfbml: true,
-     version: 'v2.4'
-   });
-	};
 
 	// adding background images
 	var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg',
 	'13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg' ];
 	var randomNumber = Math.floor(Math.random() * images.length);
-	console.log(randomNumber);
 	$scope.backgroundImage = {
 		'background-image' :'url(./Images/'+ randomNumber + '.jpg)',
 	}
@@ -93,21 +34,8 @@ app.controller("loginCtrl", function($scope,$location,$firebaseAuth,$http,$windo
 	$scope.FBlogin = function() {
 		auth.$signInWithPopup("facebook").catch(function(error){
 			console.log("Error");
-			// $scope.login = function() {
-   //    // From now on you can use the Facebook service just as Facebook api says
-   //    Facebook.login(function(response) {
-   //      console.log(response);
-    //   });
-    // };
-})
+		})
 	};
-
-	// $scope.login = function() {
- //      // From now on you can use the Facebook service just as Facebook api says
- //      Facebook.login(function(response) {
- //        console.log(response);
- //      });
- //    };
 }); //end of loginCtrl
 
 //home page controller
